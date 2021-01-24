1c427ac7d7bebae9838ea38f6db93945d9520114
Ahmet Kayra Demiryürek <demiryurekakd@gmail.com>
2021-01-24 18:38:05 +0300

    24.01.2021

---
+++ Dictionary/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> musteri = new Dictionary<string, int>();
+            musteri.Add("Hakkı",20);
+            musteri.Add("İsmail",35);
+            musteri.Add("Hilmi",18);
+            musteri.Add("Fikri",41);
+            musteri.Add("Isaac",24);
+
+
+        }
+    }
+}
